863a621a36ccb35cf2b58eeaf1b31624754402bf
Ben Price <ben@brprice.uk>
2023-01-07 01:54:58 +0000

    some clues to the problem

--- nixos/tests/vm.nix
+++ nixos/tests/vm.nix
@@ -68,6 +68,12 @@ in
     # - with vanilla nixpkgs, the test runner passes '-no-reboot' to qemu, thus killing the vm when it wants to reboot
     # - with a one word change to 'machine.py' to set 'allow_reboot = True', we cause a massive rebuild and the initial wait_for_unit("multi-user.target") never completes
     # - with also changing the qemu_opts to keep the device options, the vm runs and reboots, but the test driver errors out with `error: "invalid literal for int() with base 10: ''"` at roughly the point where in the first boot it connected to the guest root shell. I see no obvious differences in the log.
+    #   See previous commit's message for some clues
+    #   It appears that --no-reboot was first added in b1aa227cbdca8c7c17e622c8cdf89949e06e66f4 (in 2009!) to avoid qemu going into an infinite loop with a panic-ing kernel
+    #   Can I test/demonstrate this problem? (How to synthetically cause a kernel panic?)
+    #   Perhaps these days we can use the '-action' mechanism in qemu to allow reboots but halt on a panic
+    #   See https://www.qemu.org/docs/master/interop/qemu-qmp-ref.html#qapidoc-141
+    #   (however, this is beside the point for this crash - it seems that some invariant in the test driver is broken, causing it to confuse command output and command exit code (seemingly it hasn't realised that an extra line has snuck in (or out?), causing output to happen when a return code is expected and vice versa))
     name = "tmp";
     meta = with pkgs.lib.maintainers; {
       maintainers = [ brprice ];
